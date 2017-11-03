f02c250359bb80f635acda53c251e1c66eb457b3
Steve-MW <stevewaltinjoy@gmail.com>
2017-11-03 18:46:28 +0530

    Clean

--- Lab/PolynomialOperations.c
+++ Lab/PolynomialOperations.c
@@ -4,29 +4,29 @@ struct node
 {
 	int constant, power;
 	struct node *link;
-}*start1=NULL,*start2=NULL,*start3,*start4;                                                                
-void create(int ,int , struct node*);
-void add(struct node *, struct node *);
-void multiply(struct node *, struct node *);       
+}*start1=NULL,*start2=NULL,*start3=NULL,*start4=NULL;
+void create(int ,int , struct node**);
+void add();
+void multiply();
 void disp(struct node*);
 void main()
 {
 	int constant , power, totalNode1, totalNode2, choice;
 	int i;
 	struct node *temp1, *temp2;
-	         
+	
 	printf("Enter the number of elements of the first list :- ");
 	scanf("%d", &totalNode1);
 	printf("Enter the elements of the first list :- \n");
 	
-	for (i=0;i<totalNode1;i++)  
+	for (i=0;i<totalNode1;i++)
 	{
 		printf("%d. \n",i+1);
 		printf("Constant :- ");
 		scanf("%d", &constant);
 		printf("Power :- ");
 		scanf("%d",&power);
-		create(constant,power,start1);
+		create(constant,power,&start1);
 	}
 	disp(start1);
 	printf("Enter the number of elements of the second list :- ");
@@ -40,8 +40,9 @@ void main()
 		scanf("%d", &constant);
 		printf("Power :- ");
 		scanf("%d",&power);
-		create(constant,power,start2);
+		create(constant,power,&start2);
 	}
+	disp(start2);
 	
 	printf("What do you want to do ? \n");
 	printf("1.ADD\n2.MULTIPLAY\n");
@@ -49,44 +50,44 @@ void main()
 	scanf("%d",&choice);
 	switch(choice)
 	{
-		case 1: add(start1,start2);
-						//disp(start3);
+		case 1: add();
+						disp(start3);
 						break;
-		case 2: multiply(start1,start2);
-						//disp(start4);
+		case 2: multiply();
+						disp(start4);
 						break;
 		default: printf("Enter the correct choice !");
 	}
 	
 }
-void create(int constant, int power, struct node *start)
-{ printf("here");
+void create(int constant, int power, struct node **start)
+{ 
 	struct node *temp,*newNode;
 	int exit = 0;
 	newNode = (struct node*)malloc(sizeof(struct node));
 	newNode->constant=constant;
 	newNode->power=power;
-	temp=start;
+	temp=*start;
 	if(temp==NULL)
-	{ printf("here1");
-		start=newNode;
+	{ 
+		*start=newNode;
 		newNode->link=NULL;
 	}
 	else
 	{ 
 	while(temp!=NULL&&exit==0)
 		{
-		  printf("here2");
+		  
 			if(newNode->power==temp->power)
-			{ printf("here3");
+			{ 
 				temp->constant+=newNode->constant;
 				free(newNode);
 				exit=1;
 				break;
 			}
 			else if(newNode->power>temp->power)
-			{ printf("here4");
-				start=newNode;
+			{ 
+				*start=newNode;
 				newNode->link=temp;
 				exit=1;
 				break; 
@@ -95,7 +96,7 @@ void create(int constant, int power, struct node *start)
 			{ /*If we reach the last element and still, the power is not added 
 					then we just add the element to the last position 
 				*/
-				printf("here5");
+				
 				newNode->link=NULL;
 				temp->link=newNode;
 				exit=1;
@@ -105,9 +106,9 @@ void create(int constant, int power, struct node *start)
 			{ /*If we reach a postion where the last elements power is lesser
 					and the current elements power is larger then we just add the 
 					element is added in between */
-					printf("here6");
+					
 				if((temp->link)->power<newNode->power)
-				{ printf("here7");
+				{ 
 					newNode->link=temp->link;
 					temp->link=newNode;
 					exit=1;
@@ -115,7 +116,7 @@ void create(int constant, int power, struct node *start)
 				}
 				/*Or else just point to the next element */
 				else
-				{ printf("here8");
+				{ 
 					temp=temp->link;
 				}
 			}
@@ -124,73 +125,84 @@ void create(int constant, int power, struct node *start)
 	}
 	
 }
-void add(struct node *start1, struct node *start2)
-{
+void add()
+{ printf("hi");
 	struct node *temp1, *temp2, *temp3, *newNode;
 	temp1 = start1;
 	temp2 = start2;
 	temp3 = start3;
+	disp(start1);
+	disp(start2);
+	
+	
 	newNode = (struct node*)malloc(sizeof(struct node));
 	while(temp1!=NULL&&temp2!=NULL)
 	{
-		
+		printf("here");
 		if(temp1->power==temp2->power)
-		{
+		{ printf("here1");
 			newNode->constant = temp1->constant + temp2->constant;
 			newNode->power = temp1->power;
 			temp1=temp1->link;
 			temp2=temp2->link;
 		}
 		else if(temp1->power>temp2->power)
-		{
+		{ printf("here2");
 			newNode->constant = temp1->constant;
 			newNode->power = temp1->power;
 			temp1=temp1->link;
 		}
 		else if(temp1->power<temp2->power)
-		{
+		{ printf("here3");
 			newNode->constant=temp2->constant;
 			newNode->power=temp2->power;
 			temp2=temp2->link;
 		}
-		temp3->link=newNode;
+		printf("here4");
 		newNode->link=NULL;
+		temp3->link=newNode;
+		temp3=temp3->link;
 		newNode = (struct node *)malloc(sizeof(struct node));
-		/* Here we are leaving the node idle i.e. Without linking it
+		 /*Here we are leaving the node idle i.e. Without linking it
 		   to the previous node so that if at all the next while 
 		   condition is not satisfied, we can just delete it. 
 		*/
 		}
 		if(temp1==NULL&&temp2==NULL)
-		{
+		{ printf("here5");
 			free(newNode);
 		}
+		
 		else if(temp1==NULL)
-		{
+		{ printf("here6");
 			while(temp2!=NULL)	
-			{
+		      	{ printf("here7");
 				newNode->constant = temp2->constant;
 				newNode->power = temp2->power;
 				temp2=temp2->link;
-				temp3->link=newNode;
 				newNode->link=NULL;
+				temp3->link=newNode;
+				temp3=temp3->link;
 				newNode=(struct node*)malloc(sizeof(struct node ));
 			}
 		}
 		else if(temp2==NULL)
-		{
+		{ printf("here8");
 			while(temp1!=NULL)
-			{
+			{ printf("here9");
 				newNode->constant = temp1->constant;
 				newNode->power = temp1->power;
 				temp1=temp1->link;
-				temp3->link=newNode;
 				newNode->link=NULL;
+				temp3->link=newNode;
+				temp3=temp3->link;
 				newNode=(struct node*)malloc(sizeof(struct node ));
 			}
 		}
+		
+		
 }
-void multiply(struct node *start1, struct node *start2)
+void multiply()
 {
 	struct node *temp1, *temp2, *temp4, *delNode, *newNode;
 	temp1=start1;
@@ -259,7 +271,9 @@ void disp(struct node *start)
 		printf(" + %dx^%d",temp->constant,temp->power);
 		temp=temp->link;
 	}
-		
+	printf("\n");	
 }
  
+
+
 
